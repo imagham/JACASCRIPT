632b84dc6dbcb9593dc70c5b611e4c55500e53f7
imagham <151702624+imagham@users.noreply.github.com>
2024-07-30 10:48:26 +0700

    part 2

---
+++ metting 2/script.js
@@ -0,0 +1,14 @@
+// no 1
+var a;
+// no 2
+var a = null
+console.log(a);
+// no 3
+const name = "Fadgham"
+var lastname = "khairul"
+var umur = "16 tahun"
+console.log(name +' '+ lastname +" " + umur);
+// no 4
+var alas = 5
+var tinggi = 7
+console.log(1/2 * alas * tinggi);
