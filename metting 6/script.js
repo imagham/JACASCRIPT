350164f59b0789f6a7aecd592de5df2cec8a39fc
imagham <151702624+imagham@users.noreply.github.com>
2024-08-07 10:15:47 +0700

    metting 6

---
+++ metting 6/script.js
@@ -0,0 +1,26 @@
+// no 1
+const infodiri= ['Fadgham','16 Tahun','Tangerang'] 
+console.log(infodiri[1]);
+// no 2
+const namakhir= ['Fadgham','16 Tahun','Tangerang'] 
+namakhir[0]='hafizh'
+console.log(namakhir);
+// no 3
+const spilce= ['Fadgham','16 Tahun','Tangerang'] 
+spilce.splice(1,2)
+console.log(spilce);
+// no 4
+const satu= ['Fadgham','16 Tahun','Tangerang'] 
+const dua= ['SBI','17 Tahun','Tangsel'] 
+const tiga= satu.concat(dua)
+console.log(tiga);
+// no 5
+const murid=['amin','amir','umar']
+console.log(murid.indexOf('amir'));
+// no 6
+const nomor=['6','5','4','2','3','1']
+nomor.sort();
+console.log(nomor);
+// no 7
+const nomer=['6','5','4','2','3','1']
+console.log(nomer.length);
